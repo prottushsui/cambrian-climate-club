01610af657aaa7d6ac6c51ed1d3a35e606a6103f
prottushsui <prottush2011@gmail.com>
2025-11-02 22:41:02 +0600

    Create shimmer-button-demo.tsx

---
+++ src/components/ui/shimmer-button-demo.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ShimmerButton } from "@/components/ui/shimmer-button";
+
+export function ShimmerButtonDemo() {
+  return (
+    <div className="flex items-center justify-center">
+      <ShimmerButton className="shadow-2xl">
+        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+          Shimmer Button
+        </span>
+      </ShimmerButton>
+    </div>
+  );
+}
