02594c9cafe4841a9c340c4d794193652efbfc01
prottushsui <prottush2011@gmail.com>
2025-11-02 22:41:59 +0600

    Create word-pull-up-demo.tsx

---
+++ src/components/ui/word-pull-up-demo.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { WordPullUp } from "@/components/ui/word-pull-up";
+
+export function WordPullUpDemo() {
+  return (
+    <WordPullUp
+      words="Word Pull Up"
+      className="text-4xl font-bold"
+    />
+  );
+}
