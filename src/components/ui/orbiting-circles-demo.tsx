8e8da7ceed2119cf775b0aae32a0b28abbf41e32
prottushsui <prottush2011@gmail.com>
2025-11-02 22:31:06 +0600

    Add OrbitingCirclesDemo component

---
+++ src/components/ui/orbiting-circles-demo.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/ui/icons"; // Make sure Icons component exists
+import { OrbitingCircles } from "@/components/ui/orbiting-circles";
+
+function OrbitingCirclesDemo() {
+  return (
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+        Orbiting Circles
+      </span>
+
+      {/* Inner Circles */}
+      <OrbitingCircles
+        className="size-[30px] border-none bg-transparent"
+        duration={20}
+        delay={20}
+        radius={80}
+      >
+        <Icons.gitHub className="size-full text-black dark:text-white" />
+      </OrbitingCircles>
+      <OrbitingCircles
+        className="size-[30px] border-none bg-transparent"
+        duration={20}
+        delay={10}
+        radius={80}
+      >
+        <Icons.gitHub className="size-full text-black dark:text-white" />
+      </OrbitingCircles>
+
+      {/* Outer Circles (reverse) */}
+      <OrbitingCircles
+        className="size-[50px] border-none bg-transparent"
+        radius={190}
+        duration={20}
+        reverse
+      >
+        <Icons.gitHub className="size-full text-black dark:text-white" />
+      </OrbitingCircles>
+      <OrbitingCircles
+        className="size-[50px] border-none bg-transparent"
+        radius={190}
+        duration={20}
+        delay={20}
+        reverse
+      >
+        <Icons.gitHub className="size-full text-black dark:text-white" />
+      </OrbitingCircles>
+    </div>
+  );
+}
+
+export { OrbitingCirclesDemo };
