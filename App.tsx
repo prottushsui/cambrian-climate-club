db8994d8efece5b2184cc73442748dc796529420
prottushsui <prottush2011@gmail.com>
2025-11-29 20:45:12 +0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,48 @@
+
+import React from 'react';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import LeadershipPage from './pages/LeadershipPage';
+import ExecutiveCommitteePage from './pages/ExecutiveCommitteePage';
+import AdvisoryCommitteePage from './pages/AdvisoryCommitteePage';
+import ProjectsPage from './pages/ProjectsPage';
+import AchievementsPage from './pages/AchievementsPage';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+
+function App() {
+  return (
+    <HashRouter>
+      <ScrollToTop />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow pt-20">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/leadership" element={<LeadershipPage />} />
+            <Route path="/executive-committee" element={<ExecutiveCommitteePage />} />
+            <Route path="/advisory-committee" element={<AdvisoryCommitteePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/achievements" element={<AchievementsPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
